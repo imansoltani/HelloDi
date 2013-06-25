ca5449ab1d49bdd8ac44503beab7ebec0225541d
kamalkomeyli <kamal.qaen@gmail.com>
2013-06-25 07:00:41 -0700

    Retailer

--- src/HelloDi/DiDistributorsBundle/Controller/AccountController.php
+++ src/HelloDi/DiDistributorsBundle/Controller/AccountController.php
@@ -1771,7 +1771,7 @@ class AccountController extends Controller
 
         $em = $this->getDoctrine()->getManager();
         $searchForm = $this->createForm(new searchProvTransType());
-        $account = $this->container->get('security.context')->getToken()->getUser()->getAccount();
+        $account = $em->getRepository('HelloDiDiDistributorsBundle:Transaction')->find($request->get('accountid'));
         $qb = $em->createQueryBuilder()
             ->select('trans.id','trans.tranDate','trans.tranDescription','trans.tranCredit','trans.tranDebit','acc.accBalance','trans.tranAction')
             ->from('HelloDiDiDistributorsBundle:Account','acc')
@@ -1792,7 +1792,6 @@ class AccountController extends Controller
             elseif($request->get('accountid')== null){
                 $searchForm->bind($request);
                 $data = $searchForm->getData();
-
                 if($data['FromDate']!="")
                     $qb = $qb->andWhere("trans.tranDate >= :transdateFrom")->setParameter('transdateFrom', $data['FromDate']);
 
