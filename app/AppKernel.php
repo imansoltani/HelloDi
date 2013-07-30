ded0614ee2d9e14c66d5ebd4c8444a02da682e0c
MSajadi <msajadi824@gmail.com>
2013-07-30 02:55:29 -0700

    print pdf in retailer when buy

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new HelloDi\DiDistributorsBundle\HelloDiDiDistributorsBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+            new Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
