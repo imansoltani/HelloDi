30d0d53817f615e620b66cdd8d40c47a09288214
msajadi824 <msajadi824@hotmail.com>
2014-07-05 10:24:19 +0430

    fix in project (Retailer-DefaultController).
    fix dataTable: error in chrome and IE.

--- src/HelloDi/RetailerBundle/Controller/DefaultController.php
+++ src/HelloDi/RetailerBundle/Controller/DefaultController.php
@@ -1,20 +1,22 @@
 <?php
 namespace HelloDi\RetailerBundle\Controller;
 
+use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
     public function dashboardAction()
     {
+        /** @var EntityManager $em */
         $em = $this->getDoctrine()->getManager();
 
-        $Notifications = $em->getRepository('HelloDiDiDistributorsBundle:Notification')->findBy(array(
-            'Account' => $this->getUser()->getAccount()
+        $notifications = $em->getRepository('HelloDiCoreBundle:Notification')->findBy(array(
+            'account' => $this->getUser()->getAccount()
         ));
 
         return $this->render('HelloDiRetailerBundle::dashboard.html.twig', array(
-            'Notifications' => $Notifications
-        ));
+                'notifications' => $notifications
+            ));
     }
 }
