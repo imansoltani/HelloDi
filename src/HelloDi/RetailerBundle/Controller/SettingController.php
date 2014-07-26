f58e337e408bdd8c3507ba226409a7ccb581521c
msajadi824 <msajadi824@hotmail.com>
2014-07-26 11:31:36 +0430

    Retailer-setting-staff fixes

--- src/HelloDi/RetailerBundle/Controller/SettingController.php
+++ src/HelloDi/RetailerBundle/Controller/SettingController.php
@@ -2,7 +2,11 @@
 namespace HelloDi\RetailerBundle\Controller;
 
 use Doctrine\ORM\EntityManager;
+use HelloDi\AccountingBundle\Entity\Account;
+use HelloDi\CoreBundle\Entity\User;
+use HelloDi\UserBundle\Form\RegistrationFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class SettingController extends Controller
 {
@@ -17,6 +21,92 @@ class SettingController extends Controller
 
         return $this->render('HelloDiRetailerBundle:setting:profile.html.twig', array(
                 'retailer' => $retailer
+            ));
+    }
+
+    public function staffAction()
+    {
+        $users = $this->getUser()->getAccount()->getUsers();
+
+        return $this->render('HelloDiRetailerBundle:setting:staff.html.twig', array(
+                'users' => $users
+            ));
+    }
+
+    public function staffAddAction(Request $request)
+    {
+        /** @var EntityManager $em */
+        $em = $this->getDoctrine()->getManager();
+
+        $user = new User();
+
+        $languages = $this->container->getParameter('languages');
+
+        $form = $this->createForm(new RegistrationFormType($languages, Account::RETAILER), $user)
+            ->add('add','submit', array(
+                    'label'=>'Add','translation_domain'=>'common',
+                    'attr'=>array('first-button')
+                ))
+            ->add('cancel','button',array(
+                    'label'=>'Cancel','translation_domain'=>'common',
+                    'attr'=>array('onclick'=>'window.location.assign("'.$this->generateUrl('hello_di_retailer_setting_staff_index').'")','last-button')
+                ))
+        ;
+
+        if ($request->isMethod('POST')) {
+            $form->handleRequest($request);
+
+            if ($form->isValid()) {
+                $user->setEntity($this->getUser()->getEntity());
+                $user->setAccount($this->getUser()->getAccount());
+                $em->persist($user);
+                $em->flush();
+
+                $this->get('session')->getFlashBag()->add('success', $this->get('translator')->trans('the_operation_done_successfully', array(), 'message'));
+                return $this->redirect($this->generateUrl('hello_di_retailer_setting_staff_index'));
+            }
+        }
+        return $this->render('HelloDiRetailerBundle:setting:staffAdd.html.twig', array(
+                'form' => $form->createView()
+            ));
+    }
+
+    public function staffEditAction(Request $request, $user_id)
+    {
+        /** @var EntityManager $em */
+        $em = $this->getDoctrine()->getManager();
+
+        $user = $em->getRepository('HelloDiCoreBundle:User')->find($user_id);
+        if(!$user || $user->getAccount() != $this->getUser()->getAccount())
+            throw $this->createNotFoundException($this->get('translator')->trans('Unable_to_find_%object%',array('object'=>'user'),'message'));
+
+        $languages = $this->container->getParameter('languages');
+
+        $form = $this->createForm(new RegistrationFormType($languages, Account::RETAILER), $user)
+            ->remove('plainPassword')
+            ->add('update','submit', array(
+                    'label'=>'Update','translation_domain'=>'common',
+                    'attr'=>array('first-button')
+                ))
+            ->add('cancel','button',array(
+                    'label'=>'Cancel','translation_domain'=>'common',
+                    'attr'=>array('onclick'=>'window.location.assign("'.$this->generateUrl('hello_di_retailer_setting_staff_index').'")','last-button')
+                ))
+        ;
+
+        if ($request->isMethod('POST')) {
+            $form->handleRequest($request);
+
+            if ($form->isValid()) {
+                $em->flush();
+
+                $this->get('session')->getFlashBag()->add('success', $this->get('translator')->trans('the_operation_done_successfully', array(), 'message'));
+                return $this->redirect($this->generateUrl('hello_di_retailer_setting_staff_index'));
+            }
+        }
+
+        return $this->render('HelloDiRetailerBundle:setting:staffEdit.html.twig', array(
+                'form' => $form->createView()
             ));
     }
 }
