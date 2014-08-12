920af02d1690a391b2922119883c9240d42d8fad
msajadi824 <msajadi824@hotmail.com>
2014-08-12 21:43:46 +0430

    fix in deadbeat and fix history in master-code

--- src/HelloDi/MasterBundle/Controller/CodeController.php
+++ src/HelloDi/MasterBundle/Controller/CodeController.php
@@ -149,11 +149,46 @@ class CodeController extends Controller
         if (!$code)
             throw $this->createNotFoundException($this->get('translator')->trans('Unable_to_find_%object%',array('object'=>'Code'),'message'));
 
-        $removed = false;//($em->getRepository('HelloDiAccountingBundle:Transaction')->findOneBy(array('Code'=>$code,'tranAction'=>'rmv'))==null ? false : true);
+        $input_transactions = $em->createQueryBuilder()
+            ->select('transaction.date, account.name, account.type, user.firstName, user.lastName, transaction.amount, transaction.description')
+            ->from('HelloDiAggregatorBundle:Input', 'input')
+            ->innerJoin('input.codes', 'code')
+            ->where('code = :code')->setParameter('code', $code)
+            ->innerJoin('input.providerTransaction', 'transaction')
+            ->innerJoin('transaction.account', 'account')
+            ->innerJoin('input.user', 'user')
+            ->getQuery()->getArrayResult();
+
+        $pin_transactions = $em->createQueryBuilder()
+            ->select('transaction.date, account.name, account.type, user.firstName, user.lastName, transaction.amount, transaction.description')
+            ->from('HelloDiAggregatorBundle:Pin', 'pin')
+            ->innerJoin('pin.codes', 'code')
+            ->where('code = :code')->setParameter('code', $code)
+            ->innerJoin('pin.transaction', 'transaction')
+            ->innerJoin('transaction.account', 'account')
+            ->innerJoin('pin.user', 'user')
+            ->getQuery()->getArrayResult();
+
+        $pin_comm_transactions = $em->createQueryBuilder()
+            ->select('transaction.date, account.name, account.type, user.firstName, user.lastName, transaction.amount, transaction.description')
+            ->from('HelloDiAggregatorBundle:Pin', 'pin')
+            ->innerJoin('pin.codes', 'code')
+            ->where('code = :code')->setParameter('code', $code)
+            ->innerJoin('pin.commissionerTransaction', 'transaction')
+            ->innerJoin('transaction.account', 'account')
+            ->innerJoin('pin.user', 'user')
+            ->getQuery()->getArrayResult();
+
+        $transactions = array_merge($input_transactions, $pin_transactions, $pin_comm_transactions);
+
+        usort($transactions, function(array $a, array $b) {
+                if ($a['date'] == $b['date']) return 0;
+                return ($a['date'] < $b['date']) ? 1 : -1;
+            });
 
         return $this->render('HelloDiMasterBundle:code:history.html.twig', array(
             'code'=> $code,
-            'removed' => $removed
+            'transactions' => $transactions
         ));
     }
 
@@ -179,185 +214,4 @@ class CodeController extends Controller
 
         return $this->redirect($this->getRequest()->headers->get('referer'));
     }
-
-    public function CreditNoteAction(Request $request)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        die('--CreditNote--');
-//        $code = $em->getRepository('HelloDiDiDistributorsBundle:Code')->find($id);
-//        if (!$code) {
-//            throw $this->createNotFoundException($this->get('translator')->trans('Unable_to_find_%object%',array('object'=>'Code'),'message'));
-//        }
-//        if($code->getStatus()==0)
-//        {
-//            try
-//            {
-//                $user= $this->getUser();
-//                $transale = $em->getRepository('HelloDiAccountingBundle:Transaction')->findOneBy(array('Code'=>$code,'tranAction'=>'sale'),array('id'=>'desc'));
-//                $trancom = $em->getRepository('HelloDiAccountingBundle:Transaction')->findOneBy(array('Code'=>$code,'tranAction'=>'com'),array('id'=>'desc'));
-//
-//                $code->setStatus(1);
-//
-//                $trancrntsale = new Transaction();
-//                $trancrntsale->setAccount($transale->getAccount());
-//                $trancrntsale->setTranAmount(-($transale->getTranAmount()));
-//                $trancrntsale->setTranFees(0);
-//                $trancrntsale->setTranDescription('Code id is: ' . $code->getId());
-//                $trancrntsale->setTranCurrency($transale->getAccount()->getAccCurrency());
-//                $trancrntsale->setTranDate(new \DateTime('now'));
-//                $trancrntsale->setTranInsert(new \DateTime('now'));
-//                $trancrntsale->setCode($code);
-//                $trancrntsale->setTranAction('crnt');
-//                $trancrntsale->setTranType(1);
-//                $trancrntsale->setUser($user);
-//                $trancrntsale->setTranBookingValue(null);
-//                $trancrntsale->setTranBalance($transale->getAccount()->getAccBalance());
-//                $em->persist($trancrntsale);
-//
-//                $trancrntcom = new Transaction();
-//                $trancrntcom->setAccount($trancom->getAccount());
-//                $trancrntcom->setTranAmount(-($trancom->getTranAmount()));
-//                $trancrntcom->setTranFees(0);
-//                $trancrntcom->setTranDescription('Code id is: ' . $code->getId());
-//                $trancrntcom->setTranCurrency($trancom->getAccount()->getAccCurrency());
-//                $trancrntcom->setTranDate(new \DateTime('now'));
-//                $trancrntcom->setTranInsert(new \DateTime('now'));
-//                $trancrntcom->setCode($code);
-//                $trancrntcom->setTranAction('crnt');
-//                $trancrntcom->setTranType(0);
-//                $trancrntcom->setUser($user);
-//                $trancrntcom->setTranBookingValue(null);
-//                $trancrntcom->setTranBalance($trancom->getAccount()->getAccBalance());
-//                $em->persist($trancrntcom);
-//
-//                $em->flush();
-//                $this->get('session')->getFlashBag()->add('success', $this->get('translator')->trans('the_operation_done_successfully',array(),'message'));
-//            }
-//            catch(\Exception $e)
-//            {
-//                $this->get('session')->getFlashBag()->add('error', $this->get('translator')->trans('the_operation_failed',array(),'message'));
-//            }
-//        }
-//        else
-//        {
-//            $this->get('session')->getFlashBag()->add('error', $this->get('translator')->trans('code_must_be_Inactive',array(),'message'));
-//        }
-        return $this->redirect($this->getRequest()->headers->get('referer'));
-    }
-
-    public function CreditNoteAndDeadBeatAction($id)
-    {
-        $em = $this->getDoctrine()->getManager();
-        $code = $em->getRepository('HelloDiDiDistributorsBundle:Code')->find($id);
-        if (!$code) {
-            throw $this->createNotFoundException($this->get('translator')->trans('Unable_to_find_%object%',array('object'=>'Code'),'message'));
-        }
-        if($code->getStatus()==0)
-        {
-            try
-            {
-                $user= $this->getUser();
-
-                $transale = $em->getRepository('HelloDiAccountingBundle:Transaction')->findOneBy(array('Code'=>$code,'tranAction'=>'sale'),array('id'=>'desc'));
-                $trancom = $em->getRepository('HelloDiAccountingBundle:Transaction')->findOneBy(array('Code'=>$code,'tranAction'=>'com'),array('id'=>'desc'));
-
-                $trancrntsale = new Transaction();
-                $trancrntsale->setAccount($transale->getAccount());
-                $trancrntsale->setTranAmount(-($transale->getTranAmount()));
-                $trancrntsale->setTranFees(0);
-                $trancrntsale->setTranDescription('Code id is: ' . $code->getId());
-                $trancrntsale->setTranCurrency($transale->getAccount()->getAccCurrency());
-                $trancrntsale->setTranDate(new \DateTime('now'));
-                $trancrntsale->setTranInsert(new \DateTime('now'));
-                $trancrntsale->setCode($code);
-                $trancrntsale->setTranAction('crnt');
-                $trancrntsale->setTranType(1);
-                $trancrntsale->setUser($user);
-                $trancrntsale->setTranBookingValue(null);
-                $trancrntsale->setTranBalance($transale->getAccount()->getAccBalance());
-                $em->persist($trancrntsale);
-
-                $trancrntcom = new Transaction();
-                $trancrntcom->setAccount($trancom->getAccount());
-                $trancrntcom->setTranAmount(-($trancom->getTranAmount()));
-                $trancrntcom->setTranFees(0);
-                $trancrntcom->setTranDescription('Code id is: ' . $code->getId());
-                $trancrntcom->setTranCurrency($trancom->getAccount()->getAccCurrency());
-                $trancrntcom->setTranDate(new \DateTime('now'));
-                $trancrntcom->setTranInsert(new \DateTime('now'));
-                $trancrntcom->setCode($code);
-                $trancrntcom->setTranAction('crnt');
-                $trancrntcom->setTranType(0);
-                $trancrntcom->setUser($user);
-                $trancrntcom->setTranBookingValue(null);
-                $trancrntcom->setTranBalance($trancom->getAccount()->getAccBalance());
-                $em->persist($trancrntcom);
-
-                $tranadd = $em->getRepository('HelloDiAccountingBundle:Transaction')->findOneBy(array('Code'=>$code,'tranAction'=>'add'),array('id'=>'desc'));
-
-                $tranrmv = new Transaction();
-                $tranrmv->setAccount($tranadd->getAccount());
-                $tranrmv->setTranAmount(-($tranadd->getTranAmount()));
-                $tranrmv->setTranFees(0);
-                $tranrmv->setTranDescription('Code id is: ' . $code->getId());
-                $tranrmv->setTranCurrency($tranadd->getAccount()->getAccCurrency());
-                $tranrmv->setTranDate(new \DateTime('now'));
-                $tranrmv->setTranInsert(new \DateTime('now'));
-                $tranrmv->setCode($code);
-                $tranrmv->setTranAction('rmv');
-                $tranrmv->setTranType(0);
-                $tranrmv->setUser($user);
-                $tranrmv->setTranBookingValue(null);
-                $tranrmv->setTranBalance($tranadd->getAccount()->getAccBalance());
-                $em->persist($tranrmv);
-                $em->flush();
-
-                if($tranadd->getAccount()->getAccBalance()<=15000)
-                    $this->forward('hello_di_di_notification:NewAction',array('id'=>null,'type'=>12,'value'=>'15000 '.$tranadd->getAccount()->getAccCurrency().'   ('.$tranadd->getAccount()->getAccName().')'));
-
-
-                $this->get('session')->getFlashBag()->add('success', $this->get('translator')->trans('the_operation_done_successfully',array(),'message'));
-            }
-            catch(\Exception $e)
-            {
-                $this->get('session')->getFlashBag()->add('error', $this->get('translator')->trans('the_operation_failed',array(),'message'));
-            }
-        }
-        else
-        {
-            $this->get('session')->getFlashBag()->add('error', $this->get('translator')->trans('code_must_be_Inactive',array(),'message'));
-        }
-        return $this->redirect($this->getRequest()->headers->get('referer'));
-    }
-
-//    public function aaaaAction()
-//    {
-//        $em = $this->getDoctrine()->getManager();
-//
-//        $q = $em->createQueryBuilder()
-//            -> select('Distinct code.id')
-//            -> from('HelloDiDiDistributorsBundle:Code','code')
-//            -> innerJoin('code.Transactions','tr')
-//            -> where('tr.tranAction = :sale OR tr.tranAction = :rmv')->setParameter('sale','sale')->setParameter('rmv','rmv');
-//            ;
-//        $rq = $q->getQuery()->getArrayResult();
-////        die('aa');
-//        $qb=$em->createQueryBuilder();
-//        $qb -> select('code')
-//            -> from('HelloDiDiDistributorsBundle:Code','code')
-//            -> where('code.status = 0')
-//            -> andWhere('code.id NOT IN (:aa)')->setParameter('aa',$rq);
-//            ;
-//        $rqb = $qb->getQuery()->getArrayResult();
-//        $qu = $em->createQueryBuilder()
-//            ->update('HelloDiDiDistributorsBundle:Code','code')
-//            ->set('code.status',1)
-//            ->where('code.id IN (:bb)')->setParameter('bb',$rqb)
-//            ->getQuery()
-//            ->execute()
-//           ;
-//
-//        die('--'.count($qb->getQuery()->getResult()).'--');
-//    }
 }
